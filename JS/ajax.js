6ecff52a4a01d68c25d4d8651c895a75b1c58808
janderson-code <jbgfilebarbosa@gmail.com>
2020-08-04 04:55:27 -0300

    Correção ajax.js

--- JS/ajax.js
+++ JS/ajax.js
@@ -3,12 +3,13 @@ let content = document.getElementById('ajax-content')
 function fetchContent(el) {
     let view = el.getAttribute('a-view')
     let folder = el.getAttribute('a-folder')
-    fetch(`/ajax/${folder}/${view}.html´)
+    fetch(`/ajax/${folder}/${view}.html`)
     .then(response => {
       let html = response.text()
       return html
     })
     .then(html => {
+        console.log(html)
         content.innerHTML = '<p>Ola mundo</p>'
     })
 }
