6d0d2a88d89d241e81d099420a3d2cf6df699b2e
janderson-code <jbgfilebarbosa@gmail.com>
2020-08-04 05:11:49 -0300

    inserção de comentarios

--- JS/ajax.js
+++ JS/ajax.js
@@ -1,15 +1,17 @@
+/*Script para mostrar paginas dinamicas após buttons clicados que estão na div-painel-row  na pagina de gerenciamento de peso em seguida mostra paginas cadastrarPeso e visualizarPeso que estão na pasta ajax */ 
 let content = document.getElementById('ajax-content')
 
+/*Função para mostrar Html nos buttons*/
 function fetchContent(el) {
-    let view = el.getAttribute('a-view')
-    let folder = el.getAttribute('a-folder')
-    fetch(`/ajax/${folder}/${view}.html`)
-    .then(response => {
-      let html = response.text()
-      return html
+  let view = el.getAttribute('a-view') /*Variavel que pega atributo a-view e seu valor */ 
+  let folder = el.getAttribute('a-folder')/*Variavel que pega atributo a-folder e seu valor */
+  fetch(`/ajax/${folder}/${view}.html`)/*Caminho para os arquivos que estão na pagina ajax com variaveis acima*/
+    .then(response => { /*chama o then*/
+      let html = response.text()/* igual a texto*/
+      return html 
     })
-    .then(html => {
-        console.log(html)
-        content.innerHTML = '<p>Ola mundo</p>'
+    .then(html => {/*chamando a variavel html anterior */
+      console.log(html)
+      content.innerHTML = html
     })
 }
